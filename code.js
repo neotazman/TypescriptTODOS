1f7ac23db0272958af698697a5db51b869a07b1f
Tristan Alie <TristanAlie@yahoo.com>
2023-01-27 12:07:38 -0500

    receiving null as the the value

--- code.js
+++ code.js
@@ -1,1 +1,6 @@
 "use strict";
+const inputForm = document.getElementById('#new-task-title');
+const addButton = document.getElementById('#add');
+console.log(addButton);
+// addButton.addEventListener('click', () => console.log(inputForm))
+console.log(inputForm);
---
+++ code.ts
@@ -0,0 +1,6 @@
+const inputForm = document.getElementById('#new-task-title')
+const addButton = document.getElementById('#add')
+console.log(addButton)
+
+// addButton.addEventListener('click', () => console.log(inputForm))
+console.log(inputForm)
