a6082c6892fb5768fa8d5e09860301c82722f28e
Tristan Alie <TristanAlie@yahoo.com>
2023-01-30 15:32:56 -0500

    ready to create repository

--- code.js
+++ code.js
@@ -1,6 +1,10 @@
 "use strict";
-const inputForm = document.getElementById('#new-task-title');
-const addButton = document.getElementById('#add');
+const inputForm = document.querySelector('#new-task-title');
+const addButton = document.getElementById('add');
 console.log(addButton);
 // addButton.addEventListener('click', () => console.log(inputForm))
 console.log(inputForm);
+addButton === null || addButton === void 0 ? void 0 : addButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(e);
+});
--- code.ts
+++ code.ts
@@ -1,6 +1,13 @@
-const inputForm = document.getElementById('#new-task-title')
-const addButton = document.getElementById('#add')
+type Inputs = 'text' | 'submit'
+
+const inputForm = document.querySelector('#new-task-title')
+const addButton : HTMLElement | null = document.getElementById('add')
 console.log(addButton)
 
 // addButton.addEventListener('click', () => console.log(inputForm))
 console.log(inputForm)
+
+addButton?.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(e)
+})
