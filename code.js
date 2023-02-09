f0ff604aa18506fd4b8af29d7fc0997f3a6f70a4
Tristan Alie <TristanAlie@yahoo.com>
2023-02-09 17:44:19 -0500

    finished description and gave everything its own line

--- code.js
+++ code.js
@@ -3,30 +3,32 @@ const inputForm = document.getElementById('new-task-form');
 const addButton = document.getElementById('add');
 const prioritySelector = document.getElementById('priority');
 const deadlineStr = document.getElementById('deadline');
+const descriptionStr = document.getElementById('description');
 const displayContainer = document.getElementById('task-list');
 // console.log(addButton)
 const tasks = [];
 class CreateNewToDoItem {
-    constructor(name, created, priority, deadline) {
+    constructor(name, created, priority, deadline, description) {
         this.name = name;
         this.created = created;
         this.priority = priority;
         this.deadline = deadline || '';
+        this.description = description || '';
     }
 }
 function displayTasks() {
     displayContainer.innerHTML = '';
     tasks.forEach((task, i) => {
-        let isThereADeadline = task.deadline ? ' deadline: ' + task.deadline : '';
-        displayContainer === null || displayContainer === void 0 ? void 0 : displayContainer.innerHTML += `<div class=\"task-${i}\">${task.name}: created: ${task.created} priority: ${task.priority}${isThereADeadline}</div>`;
+        let isThereADeadline = task.deadline ? `<div class=\"task-${i}\">deadline: ${task.deadline}</div>` : '';
+        let isThereADescription = task.description ? `<div class=\"task-${i}\">description: ${task.description}</div>` : '';
+        displayContainer === null || displayContainer === void 0 ? void 0 : displayContainer.innerHTML += `<div class=\"task-${i}\">${task.name}: </div><div class=\"task-${i}\">created: ${task.created} </div><div class=\"task-${i}\">priority: ${task.priority}${isThereADeadline}</div>${isThereADescription}`;
     });
 }
 addButton.addEventListener('click', (e) => {
     e.preventDefault();
     let textBox = inputForm.children[1].value;
-    let task = new CreateNewToDoItem(textBox, new Date().toString().split('G')[0], prioritySelector.value, deadlineStr === null || deadlineStr === void 0 ? void 0 : deadlineStr.value);
+    let task = new CreateNewToDoItem(textBox, new Date().toString().split('G')[0], prioritySelector.value, deadlineStr === null || deadlineStr === void 0 ? void 0 : deadlineStr.value, descriptionStr === null || descriptionStr === void 0 ? void 0 : descriptionStr.value);
     tasks.push(task);
-    console.log(deadlineStr.value);
     console.table(tasks);
     displayTasks();
 });
--- code.ts
+++ code.ts
@@ -1,4 +1,4 @@
-type Inputs = 'text' | 'submit'
+// type Inputs = 'text' | 'submit' never used this
 type PriorityLevel = 'Low' | 'Medium' | 'High' // keeping the next line in case i need it later 
 // enum PriorityLevel { LOW = 'Low', MEDIUM = 'Medium', HIGH = 'High'} // in the end, i could've just used type instead of enum
 
@@ -14,6 +14,7 @@ const inputForm : HTMLElement | null= document.getElementById('new-task-form')
 const addButton : HTMLElement | null = document.getElementById('add')
 const prioritySelector : HTMLElement | null = document.getElementById('priority')
 const deadlineStr : HTMLElement | null = document.getElementById('deadline')
+const descriptionStr : HTMLElement | null = document.getElementById('description')
 const displayContainer : HTMLElement | null = document.getElementById('task-list')
 // console.log(addButton)
 
@@ -23,29 +24,31 @@ class CreateNewToDoItem implements ToDoItem {
     name: string
     created: string
     priority: PriorityLevel
-    deadline?: string | undefined;
-    constructor(name: string, created: string, priority: PriorityLevel, deadline?: string) {
+    deadline?: string | undefined
+    description?: string | undefined
+    constructor(name: string, created: string, priority: PriorityLevel, deadline?: string, description?: string) {
         this.name = name
         this.created = created
         this.priority = priority
         this.deadline = deadline || ''
+        this.description = description || ''
     }
 }
 
 function displayTasks(): void {
     displayContainer!.innerHTML = ''
     tasks.forEach((task, i) => {
-        let isThereADeadline = task.deadline ? ' deadline: ' + task.deadline : ''
-        displayContainer?.innerHTML+= `<div class=\"task-${i}\">${task.name}: created: ${task.created} priority: ${task.priority}${isThereADeadline}</div>`
+        let isThereADeadline = task.deadline ? `<div class=\"task-${i}\">deadline: ${task.deadline}</div>` : ''
+        let isThereADescription = task.description ? `<div class=\"task-${i}\">description: ${task.description}</div>` : ''
+        displayContainer?.innerHTML+= `<div class=\"task-${i}\">${task.name}: </div><div class=\"task-${i}\">created: ${task.created} </div><div class=\"task-${i}\">priority: ${task.priority}${isThereADeadline}</div>${isThereADescription}`
     })
 }
 
 addButton!.addEventListener('click', (e) => {
     e.preventDefault()
     let textBox: string = inputForm!.children[1].value
-    let task = new CreateNewToDoItem (textBox, new Date().toString().split('G')[0], prioritySelector!.value, deadlineStr?.value)
+    let task = new CreateNewToDoItem (textBox, new Date().toString().split('G')[0], prioritySelector!.value, deadlineStr?.value, descriptionStr?.value)
     tasks.push(task)
-    console.log(deadlineStr.value)
     console.table(tasks)
     displayTasks()
 })
